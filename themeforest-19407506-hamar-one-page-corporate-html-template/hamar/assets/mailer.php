461659ab18c11012ed6b983db7d729fb90fbba90
Oriol Romani <oriol.romani@upf.edu>
2017-09-08 17:21:44 +0200

    Email in form

--- themeforest-19407506-hamar-one-page-corporate-html-template/hamar/assets/mailer.php
+++ themeforest-19407506-hamar-one-page-corporate-html-template/hamar/assets/mailer.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "yourname@domain.com";
+        $recipient = "oriolromanipicas@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
